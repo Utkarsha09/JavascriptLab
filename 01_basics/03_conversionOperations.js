bc787ad75bab6e3654eed1b65009e97def82ce12
Utkarsha Shirbhate <123170380+Utkarsha09@users.noreply.github.com>
2024-01-15 06:54:13 +0000

    Conversion and Operation of JS

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -1,34 +1,74 @@
-let score = "33abc"
+ let score = "33abc"
 
-// Both are same
-console.log(typeof score);
-console.log(typeof(score)); // In functions
+ // Both are same
+ console.log(typeof score);
+ console.log(typeof(score)); // In functions
 
-// Conversion string to number
-let valueInNumber = Number(score);
-console.log(typeof valueInNumber);
-console.log(valueInNumber); // NaN:- Can't convert properly
-
-
-// "33 :- 33
-//"33abc" :- NaN
-// true :- 1, False :- 0
+ // Conversion string to number
+ let valueInNumber = Number(score);
+ console.log(typeof valueInNumber);
+ console.log(valueInNumber); // NaN:- Can't convert properly
 
 
-let isloggedIn = 1
+ // "33 :- 33
+ //"33abc" :- NaN
+ // true :- 1, False :- 0
 
-let booleanisloggedIn = Boolean(isloggedIn)
 
-console.log(isloggedIn);
-console.log(booleanisloggedIn);
+ let isloggedIn = 1
 
-// 1 :- true , 0 :- false
-// "" :- false
-// "Utkarsha" :- true
+ let booleanisloggedIn = Boolean(isloggedIn)
 
-let someNumber = 66
+ console.log(isloggedIn);
+ console.log(booleanisloggedIn);
 
-let stringNumber = String(someNumber);
+ // 1 :- true , 0 :- false
+ // "" :- false
+ // "Utkarsha" :- true
 
-console.log(stringNumber);
-console.log(typeof stringNumber);
+ let someNumber = 66
+
+ let stringNumber = String(someNumber);
+
+ console.log(stringNumber);
+ console.log(typeof stringNumber);
+
+
+ // ************** Operation *****************
+
+let value =9
+let negValue = -value
+console.log(negValue);
+
+console.log(2+3);
+console.log(2-2);
+console.log(2*2);
+console.log(2**2);
+console.log(2/4);
+console.log(2%3);
+
+let str1 ="Hello"
+let str2 = "Utkarsha"
+
+let str3 = str1+str2
+console.log(str3);
+
+console.log("1"+ 2);
+console.log(1+ "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+
+console.log(+true); // 1
+console.log(+""); // 0 not neccecary 
+
+let num1, num2, num3 
+
+num1 = num2 = num3 = 2+2
+
+console.log(num1);
+console.log(num2);
+console.log(num3);
+
+let gamecounter =100
+++gamecounter;
+console.log(gamecounter);
