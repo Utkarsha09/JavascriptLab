d6917d75da7ddffdf49a25f23f1851a763754774
Utkarsha Shirbhate <123170380+Utkarsha09@users.noreply.github.com>
2024-01-15 16:00:45 +0000

    DataTypes of JS

--- 01_basics/02_Datatypes.js
+++ 01_basics/02_Datatypes.js
@@ -9,6 +9,14 @@ let name= "Utkarsha"
 let age = 21
 let IsloggedIn = true
 
+const score = 100
+const scoreValue = 100.3
+
+const IsloggedIn1 = false
+const outsideTemp = null
+
+let userEmail;
+
 // ** Premitive DataType **
 // number :- 2 to power 53
 // bigint :- To Long number
@@ -18,8 +26,31 @@ let IsloggedIn = true
 // undefined :- No value is given * one of the Data Types *
 // Symbol :- Indentify to unquie components
 
+const id = Symbol('123')
+const anotherId = Symbol('123')
+console.log(id === anotherId);
+
+const bigNumber = 34556667211113399000n // bigint type
 
 // ** Object 
 
 console.log(typeof null); // Oject
 console.log(typeof undefine); // undefine
+
+// Non-Primitive
+
+// Array, Objects, Functions
+
+const heros = ["shaktiman","spiderman","ironman"] // Array
+
+let myObj = {
+    name: "Utkarsha",
+    age: 22
+} // Oject
+
+const myfunction=function(){
+    console.log("Hello World!!");
+}// Function
+
+console.log(typeof myfunction);
+
