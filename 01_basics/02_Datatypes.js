611df232ead5cbfa7942e1925da290a14807d0ac
Utkarsha Shirbhate <123170380+Utkarsha09@users.noreply.github.com>
2024-01-15 09:41:37 +0530

    Create 02_Datatypes.js

---
+++ 01_basics/02_Datatypes.js
@@ -0,0 +1,24 @@
+ "use strict" // treat all JS code as newer version
+
+//  alert("Hello") // We are Using nodejs, not browser
+
+console.log(3 +3);
+
+let name= "Utkarsha"
+let age = 21
+let IsloggedIn = true
+
+// ** Premitive DataType **
+// number :- 2 to power 53
+// bigint :- To Long number
+// String :- we can use ""/ ''
+// Boolean :- True/False
+// null :- it is one of the satandalone value * it is Object *
+// undefined :- No value is given * one of the Data Types *
+// Symbol :- Indentify to unquie components
+
+
+// ** Object 
+
+console.log(typeof null); // Oject
+console.log(typeof undefine); // undefine
