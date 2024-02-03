54c2a5567e92eeac2fd13bac75baf67595abd0a3
Utkarsha Shirbhate <123170380+Utkarsha09@users.noreply.github.com>
2024-02-03 06:40:59 +0000

    String in JS

--- 01_basics/06_string.js
+++ 01_basics/06_string.js
@@ -1,22 +1,23 @@
 const name = "Utkarsha"
 const repoCount = 50
-consol.log(name + repoCount + "value"); // don't used 
+console.log(name + repoCount + "value"); // don't used 
 
-consol.log('Hello my namr is ${name} and my repo count is ${repoCount}');
+console.log(`Hello my namr is ${name} and my repo count is ${repoCount}`);
 
  //Another way to diclear string
 const gameName = new String('UtkashaSS')
-consol.log(gameName[0]);
-consol.log(gameName.__proto__);
+console.log(gameName[0]);
+console.log(gameName.__proto__);
 
-consol.log(gameName.length);
-consol.log(gameName.toUpperCase());
-consol.log(gameName.charAt(3));// display charater in string in which index no.
-consol.log(gameName.indexOf('t'));// display charater in string which present in given index no.
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(3));// display charater in string in which index no.
+console.log(gameName.indexOf('t'));// display charater in string which present in given index no.
 
 //Substring
  const newString = gameName.substring(0,4)
  console.log(newString);
+ 
 //slice
 const anotherstring = gameName.slice(-8,4)
 console.log(anotherstring);
@@ -25,13 +26,14 @@ console.log(anotherstring);
 const str1 = "  utkarsha   "
 console.log(str1);
 console. log(str1.trim());
+
 //replace
 const url = "https://utkarsha.com/utkarsha%09shirbhate"
 console.log(url.replace('%09', '-'));
 console.log(url.includes('utkarsha'));// check the string present or not 
 
 //split
-const gName = "I am a future Engineer: 
+const gName = "I am a future Engineer" 
 console.log(gName.split(" "));
 
 
