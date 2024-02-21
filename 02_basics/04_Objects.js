bc7afa6fdaa9b29ce77d81f47b31125df6d25842
Utkarsha Shirbhate <123170380+Utkarsha09@users.noreply.github.com>
2024-02-21 12:30:19 +0000

    Object access in JS

---
+++ 02_basics/04_Objects.js
@@ -0,0 +1,40 @@
+// const tinderUser = new Object() // singleton object
+const tinderUser2 = {} // Non-singleton object
+
+tinderUser2.id = "123utk"
+tinderUser2.name = "Abhi"
+tinderUser2.isLoggedIn = false
+
+console.log(tinderUser2);
+console.log(Object.keys(tinderUser2)); // display all keys in object in the form of array
+console.log(Object.values(tinderUser2));// display all values in object in form of array
+console.log(Object.entries(tinderUser2));// covert object'skeys and values in the form of array
+// to check wethere object has declared propertis or not
+console.log(tinderUser2.hasOwnProperty('isLoggedIn'));//display in boolean
+
+// how to decleared object under objects
+
+const regularUser = {
+    email : "utkarsha@gmail.com",
+    fullname: {
+        userfullname : {
+            firstname : "Utkarsha",
+            lastname : "Shirbhate"
+        }
+    }
+}
+
+console.log(regularUser);
+
+//How to access object under object
+console.log(regularUser.fullname.userfullname);
+console.log(regularUser.fullname.userfullname.firstname);
+
+const obj1 = {1:"a",2:"b"}
+const obj2 = {3:"a",4:"b"}
+
+// const obje3 = Object.assign(obj1,obj2)// all object merge in obje1
+// const obje3 = Object.assign({},obj1,obj2) // if there is more number of objects
+// merge object by using spred operator(...)
+const obje3 = {...obj1, ...obj2}
+console.log(obje3);
